0d76c9d908f0f8190ca95db3b0adc21e8250285c
JesseShih470 <147682761+JesseShih470@users.noreply.github.com>
2026-08-27 12:40:08 +1200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Fill these three values after creating the free Supabase project.
 window.NOOK_CONFIG = {
   supabaseUrl: 'https://gvdwnuqmozunrfvqmyar.supabase.co',
-  supabaseAnonKey: 'PASTE_YOUR_SUPABASE_ANON_KEY_HERE',
+  supabaseAnonKey: 'sb_publishable_9vyNam_1abQJPBa-1ICl8w_TvAu6pAS',
   adminEmail: 'jesseshih470@gmail.com'
 };
